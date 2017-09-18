0dbd2dfa3eee804569aba2aa5a92fe5da252a680
Ashley-P <ashleyp1244@hotmail.co.uk>
2017-09-18 22:17:14 +0100

    Set up the class for the Cursor and set up a dummy class for the buffer

--- editor.py
+++ editor.py
@@ -7,7 +7,42 @@ SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
 
-# Initialisation & Main Loop
+class Buffer(object):
+    '''Gets pushed to the window and then blitted to the console'''
+    pass
+
+
+class Cursor(object):
+    '''Dictates where the characters are placed'''
+
+    def __init__(self, x, y, console):
+        self.x = x
+        self.y = y
+        self.char = 221
+        self.colour = 0xFFFFFF
+        self.con = console
+
+    def move(self, dx, dy):
+        # Moves the cursor by the given amount
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        # Draws the cursor
+        self.con.draw_char(self.x, self.y, self.char, self.colour, bg=None)
+
+    def clear(self):
+        # Erase the cursor
+        self.con.draw_char(self.x, self.y, ' ', self.colour, bg=None)
+
+
+def keybinds():
+
+    pass
+
+##############################
+# Initialisation & Main Loop #
+##############################
 
 def main():
     tdl.set_font('terminal16x16_gs_ro.png', greyscale=True, altLayout=False)
@@ -15,8 +50,17 @@ def main():
     tdl.setFPS(LIMIT_FPS)
     con = tdl.Console(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+    cursor = Cursor(0, 0, con)
+
     while not tdl.event.is_window_closed():
-        pass
+
+        cursor.draw()
+
+        root.blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0)
+        tdl.flush()
+
+        cursor.clear()
+
 
 if __name__ == "__main__":
     main()
